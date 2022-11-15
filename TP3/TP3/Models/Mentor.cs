07bacc86a8e861dd09c20bbea54589189330a9d5
MaxaimBardin <bmaximss2003@gmail.com>
2022-11-16 00:33:47 +0300

    Create Mentor.cs

---
+++ TP3/TP3/Models/Mentor.cs
@@ -0,0 +1,12 @@
+﻿namespace TP3.Models
+{
+    public class Mentor: Base
+    {
+        public string FIO { get; set; }
+        public string Post { get; set; }
+
+        public int ClientId { get; set; }
+
+        public virtual Guid client { get; set; }
+    }
+}
