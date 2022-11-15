78ad495834c6feb10afdf94086afe4f7dee0ceaa
MaxaimBardin <bmaximss2003@gmail.com>
2022-11-16 00:32:46 +0300

    Create AppContext.cs

---
+++ TP3/TP3/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using TP3.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+
+namespace TP3
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Client> Client { get; set; }
+        public DbSet<Mentor> Mentor { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
